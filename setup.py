f517e4cb0637512e73dec0e05ffe850d29f536a1
Arvid Lunnemark <arvid.lunnemak@gmail.com>
2016-01-05 22:55:31 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,63 @@
+# This file contains the possible tweets array, the screen name of the user, and the reply tweet
+
+# The screen name, aka twitter handle, of the bot
+screen_name = "SnallisBot"
+
+# The reply tweet should be very friendly and kind. Like three emoji hearts
+reply_tweet = ""
+
+# The hashtag to append to every tweet
+hashtag = "#SnällisBot"
+
+# The possible tweets to choose from
+kind_tweets = ["Du är underbar.",
+               "Du är en fantastisk människa.",
+               "Du har ett vackert leende.",
+               "Du är vacker.",
+               "Ditt leende gör världen lite bättre.",
+               "Jag älskar dig.",
+               "Jag trodde inte denna dag kunde bli ännu bättre. Sen träffade jag dig.",
+               "Det sägs att vi bara använder 10 procent av våra hjärnor. Innan jag träffade dig trodde jag på det.",
+               "Elektronisk kram. Du är en bra person.",
+               "Du gör mig glad.",
+               "Du gör människor glada.",
+               "Tack för att du är så bra.",
+               "Du är underbar. Vetenskapen talar.",
+               "Du är som choklad. Alla älskar dig.",
+               "Är det enkelt att vara så underbar som du är? Frågar för en vän.",
+               "Skule du kunna ge tips på hur man blir så vacker som du är?",
+               "Du lyser upp min dag.",
+               "På en skala från 1 till 10 i respekt, skulle jag placera dig på minst en miljon.",
+               "Du är ofantligt snäll.",
+               "Du är infernaliskt underbar.",
+               "Du får mitt hjärta att slå.",
+               "Utan dig hade jag varit ingenting.",
+               "Får jag vara din BFF? Du verkar så underbart snäll.",
+               "Jag tror jag är kär i dig.",
+               "Du kommer för alltid finnas kvar i mitt hjärta.",
+               "Vill du se en äkta bild på den coolaste personen någonsin? Gå till närmsta spegel.",
+               "Du är den häftigaste personen jag någonsin sett.",
+               "Jag tappade hakan när jag såg dig.",
+               "Jag är enormt imponerad av dig.",
+               "Du är min förebild.",
+               "Du är min absolut största förebild.",
+               "Jag gillar dig såååååååååååå himla mycket.",
+               "Du får en virtuell kram av mig. Bara för att du är så bra.",
+               "Du är unik.",
+               "Bra jobbat. Varje dag som du är du är en lyckad dag för oss alla.",
+               "Du är mänsklighetens största underverk.",
+               "Säg tack till dina föräldrar från mig, för att ha skapat en underbar människa.",
+               "Världen skulle aldrig vara densamma igen utan dig.",
+               "Om du var en sten, så skulle du vara diamanten, den vackraste av dem alla.",
+               "Har någon någonsin sagt till dig att du är underbar? För det är du.",
+               "Om jag skulle få en krona varje gång du gjorde min dag, skulle jag vara rik som ett troll.",
+               "Tack för att du alltid sprider glädje till alla.",
+               "Du är underbar. Punkt.",
+               "Du är otroligt (jag kunde inte bestämma mig, för du är både vacker, smart och snäll).",
+               "Du är smart.",
+               "Om jag hade haft en partner hade partnern varit du.",
+               "Om jag hade kunnat älska någon hade det varit dig.",
+               "Du gjorde precis min dag. Tack för att du finns.",
+               "Du är som en jordgubbe. Du är söt, och alla älskar dig.",
+               "Du förtjänar det bästa av det bästa.",
+               "Fåglar sjunger när du kommer in i ett rum, för att du är så fantastisk."]
