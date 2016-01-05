1249041a5c57c3df33ca7b5bf3bf615671093297
Arvid Lunnemark <arvid.lunnemak@gmail.com>
2016-01-06 00:07:34 +0100

    Updated twythonaccess to accomodate for two apps

--- twythonaccess.py
+++ twythonaccess.py
@@ -2,26 +2,53 @@
 
 # import time, to enable the sleep function
 import time
-
 # Import twython
 from twython import Twython
-
 # import the api keys
 from . import apikeys
+# import setup to have the screen_name available
+from . import setup
+# import threading to schedule reset
+from threading import Timer
+# import datetime
+from datetime import datetime
 
 
+
+
+# store the number of reuests for each application
+main_requests_since_last_sleep = 0
+mine_requests_since_last_sleep = 0
+
+# neither is sleeping at start
+main_is_sleeping = False
+mine_is_sleeping = False
+
+# time of last request is used t be able t reset the requests
+# utc time is reliable
+main_time_of_last_request = datetime.utcnow()
+mine_time_of_last_request = datetime.utcnow()
 
 
 # The api variable is the way to access the api
-def authorize():
-    return Twython(apikeys.CONSUMER_KEY, apikeys.CONSUMER_SECRET, apikeys.ACCESS_TOKEN, apikeys.ACCESS_TOKEN_SECRET)
+# there are two authorization possibilities: main or mine application
+def authorize(main=True):
+    # if more than 16 minutes have elapsed since the last request, the reqests limit can be reset
+    check_if_requests_can_be_reset(main)
+    if main:
+        global main_requests_since_last_sleep
+        main_requests_since_last_sleep += 1
+        # authorize
+        return Twython(apikeys.CONSUMER_KEY, apikeys.CONSUMER_SECRET, apikeys.ACCESS_TOKEN, apikeys.ACCESS_TOKEN_SECRET)
+    else:
+        global mine_requests_since_last_sleep
+        mine_requests_since_last_sleep += 1
+        # authorize
+        return Twython(apikeys.MINE_CONSUMER_KEY, apikeys.MINE_CONSUMER_SECRET, apikeys.MINE_ACCESS_TOKEN, apikeys.MINE_ACCESS_TOKEN_SECRET)
 
-# the screen name for self
-screen_name = "gsgottsnack"
 
 # this method sends a tweet, by first checking with me
 def send_tweet(tweet, in_reply_to_status_id=0):
-    global screen_name
     # send the tweet first as a dm to arvid220u
     # if he replies with yes, send the tweet.
     # if he replies with no, don't send the tweet (generate a new one)
@@ -30,14 +57,15 @@ def send_tweet(tweet, in_reply_to_status_id=0):
     # REMOVING CONFIRMATION VIA DM, BECAUSE TWEET GENERATION HAS A HIGH LOWEST QUALITY NOW
     
     # send tweet
-    check_if_requests_are_maximum(13)
+    # only main application is allowed to send tweets
+    sleep_if_requests_are_maximum(14, main=True)
     # maybe send it in reply to another tweet
     if in_reply_to_status_id == 0:
         # standalone tweet
-        authorize().update_status(status=tweet)
+        authorize(main=True).update_status(status=tweet)
     else:
         # tweet is a reply
-        authorize().update_status(status=tweet, in_reply_to_status_id=in_reply_to_status_id)
+        authorize(main=True).update_status(status=tweet, in_reply_to_status_id=in_reply_to_status_id)
     print("sent tweet: " + tweet)
 
     # RETURN TRUE HERE SO DM WON'T BE SENT. TRUST GSGOTTSNACK.
@@ -96,19 +124,61 @@ def send_tweet(tweet, in_reply_to_status_id=0):
     # return true, meaning this function should not be called again
     return True
 
-# Store number of requests, so that they won't exceed 14
-requests_since_last_sleep = 0
+
 # This method is called everytime a request is to be made
 # IF the requests variable is over limit, then it sleeps for 17 minutes
 # if the requests variable isn't over limit, then increment it by one
-def check_if_requests_are_maximum(limit):
-    global requests_since_last_sleep
-    print("Requests since last sleep: " + str(requests_since_last_sleep))
-    if requests_since_last_sleep >= limit:
-        print("will sleep")
-        time.sleep(16*60)
-        print("has slept")
-        # reset requests
-        requests_since_last_sleep = 0
-    # increment requests
-    requests_since_last_sleep += 1
+def sleep_if_requests_are_maximum(limit, main=True):
+    if main:
+        global main_requests_since_last_sleep
+        print("Requests since last sleep: " + str(main_requests_since_last_sleep))
+        if main_requests_since_last_sleep >= limit:
+            if not main_is_sleeping:
+                main_is_sleeping = True
+                # reset after 16 minutes
+                Timer(16*60, main_reset_requests).start()
+        # if main is sleeping
+        if main_is_sleeping:
+            print("will sleep")
+            time.sleep(16*60)
+            print("has slept")
+    else:
+        global mine_requests_since_last_sleep
+        print("Requests since last sleep: " + str(mine_requests_since_last_sleep))
+        if mine_requests_since_last_sleep >= limit:
+            if not mine_is_sleeping:
+                mine_is_sleeping = True
+                # reset after 16 minutes
+                Timer(16*60, mine_reset_requests).start()
+        # if main is sleeping
+        if mine_is_sleeping:
+            print("will sleep")
+            time.sleep(16*60)
+            print("has slept")
+
+
+# if more than 16 minutes have elapsed since the last request, safely reset the count
+def check_if_requests_can_be_reset(main=True):
+    now_time = datetime.utcnow()
+    if main:
+        global main_time_of_last_request
+        if (now_time - main_time_of_last_request).total_seconds() > 16*60:
+            global main_requests_since_last_sleep
+            main_requests_since_last_sleep = 0
+        main_time_of_last_request = now_time
+    else:
+        global mine_time_of_last_request
+        if (now_time - mine_time_of_last_request).total_seconds() > 16*60:
+            global mine_requests_since_last_sleep
+            mine_requests_since_last_sleep = 0
+        mine_time_of_last_request = now_time
+
+
+# reset the main requests
+def main_reset_requests():
+    main_requests_since_last_sleep = 0
+    main_is_sleeping = False
+# reset the mine requests
+def mine_reset_requests():
+    mine_requests_since_last_sleep = 0
+    mine_is_sleeping = False
